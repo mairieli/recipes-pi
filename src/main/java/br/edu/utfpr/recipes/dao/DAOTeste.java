9cc872f8a8c562e3b9b613140341cac745bb5772
Mairieli Wessel <mairieliw@gmail.com>
2016-04-08 17:59:32 -0300

    pacotes adicionados

---
+++ src/main/java/br/edu/utfpr/recipes/dao/DAOTeste.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.recipes.dao;
+
+/**
+ *
+ * @author mairieliw
+ */
+public class DAOTeste {
+    
+}
---
+++ src/main/java/br/edu/utfpr/recipes/entidade/EntidadeTeste.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.recipes.entidade;
+
+/**
+ *
+ * @author mairieliw
+ */
+public class EntidadeTeste {
+    
+}
