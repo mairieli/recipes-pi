b5003e549de2ddb3d805054e48cee80ff69aab06
Mairieli Wessel <mairieliw@gmail.com>
2016-04-12 20:22:28 -0300

    adicionado DaoReceita #7

---
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.recipes.dao;
+
+import br.edu.utfpr.recipes.entidade.Receita;
+
+/**
+ *
+ * @author mairieliw
+ */
+public class DaoReceita extends DaoGenerics<Receita>{
+
+    public DaoReceita() {
+        super.clazz = Receita.class;
+    }
+    
+    
+}
