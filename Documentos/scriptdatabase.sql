ca6888add51cb7b6565a57e2b76407c1c9f42aa4
Quejao <Leotrone@hotmail.com>
2016-04-14 22:56:41 -0300

    adicionado o novo scriptdatabase

---
+++ Documentos/scriptdatabase.sql
@@ -0,0 +1,107 @@
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Micro-ondas', 1);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Forno', 2);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Fogão', 3);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Freezer', 4);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Liquidificador', 5);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Batedeira', 6);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Processador', 7);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Grill', 8);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Sanduicheira', 9);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Cafeteira', 10);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Colher de café', 11);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Colher de chá', 12);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Colher de sopa', 13);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Garfo', 14);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Copo', 15);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Taça', 16);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Espátula', 17);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Xícara', 18);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Concha', 19);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Descascador de legumes', 20);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Escorredor de macarrão', 21);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Escumadeira', 22);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Faca', 23);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Fouet', 24);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Tigela', 25);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Prato', 26);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Panela de pressão', 27);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Panela', 28);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Frigideira', 29);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Peneira', 30);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Chaleira', 31);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Ralador', 32);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Funil', 33);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Tábua', 34);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Descascador', 35);
+
+INSERT INTO `mydb`.`Tag`(nome, id)
+VALUES ('Forma', 36);
