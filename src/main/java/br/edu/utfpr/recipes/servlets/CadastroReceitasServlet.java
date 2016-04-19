028a5cf8e2e7710e59e62d58637b9539985dc04b
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-04-19 20:41:20 -0300

    Capturar Ingredientes da receita no servlet #33

--- src/main/java/br/edu/utfpr/recipes/servlets/CadastroReceitasServlet.java
+++ src/main/java/br/edu/utfpr/recipes/servlets/CadastroReceitasServlet.java
@@ -5,7 +5,9 @@ import br.edu.utfpr.recipes.entidade.Ingrediente;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,17 +29,34 @@ public class CadastroReceitasServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        request.setCharacterEncoding("UTF-8");
+
+        // Captura Quantidades. Unidade de Medidas e Ingredientes
+        List<String> Quantidades = new LinkedList<>();
+        List<String> Unidades = new LinkedList<>();
+        List<String> Ingredientes = new LinkedList<>();
+        Map<String, String[]> parametrosRecebidos = request.getParameterMap();
+        for (Map.Entry<String, String[]> entrada : parametrosRecebidos.entrySet()) {
+            String parametro = entrada.getKey();
+            if (parametro.startsWith("quantidade")) {
+                Integer i = Integer.parseInt(parametro.substring(parametro.length() - 1));
+                Quantidades.add(request.getParameter("quantidade" + i));
+                Unidades.add(request.getParameter("unidade_medida" + i));
+                Ingredientes.add(request.getParameter("ingrediente" + i));
+            }
+        }
+        // Fim da captura de Quantidades. Unidade de Medidas e Ingredientes.
+
         //mostra qual tags foram marcadas
         String[] tag = request.getParameterValues("tag");
         ArrayList<String> listaTag = new ArrayList<String>();
         listaTag.addAll(Arrays.asList(tag));
         System.out.println(Arrays.toString(listaTag.toArray()));
-        
+
         //exemplo de como usar o método que recupera todos os ingredientes da receita
         //String[] nomes = {"Cebolinha", "Calabresa", "páprica verde"};
         //List<Ingrediente> listaIngrediente = new LinkedList<>();
         //recuperaIngredientesReceita(nomes, listaIngrediente);
-
         response.sendRedirect("CadastroReceitas.jsp");
     }
 
