e72029bd6ab032794eab5b2a0c9034b4eb9c3f80
noemis13 <noemischerer13@gmail.com>
2016-04-25 00:10:41 -0300

    arrumando o buscaDaoReceita #16

--- src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
@@ -1,6 +1,9 @@
 package br.edu.utfpr.recipes.dao;
 
+import br.edu.utfpr.recipes.entidade.Ingrediente;
 import br.edu.utfpr.recipes.entidade.Receita;
+import java.util.ArrayList;
+import java.util.List;
 import org.hibernate.Query;
 
 /**
@@ -13,13 +16,12 @@ public class DaoReceita extends DaoGenerics<Receita> {
         super.clazz = Receita.class;
     }
 
-    public Receita BuscaIngredienteEspecificado(String nomeIngrediente) {
-        Receita receita = null;
+    public ArrayList<Receita> BuscaIngredienteEspecificado(ArrayList<Ingrediente> nomeIngrediente) {
+        ArrayList<Receita> receita = null;
         session = getsession();
         Query query = session.createQuery("From " + clazz.getSimpleName() + " where lower(nome) =:nome ");
-        receita = (Receita) query.setParameter("nome", nomeIngrediente.toLowerCase()).uniqueResult();
-
-        return receita;
+        receita = (ArrayList<Receita>) query.setParameter("nome", nomeIngrediente).uniqueResult();
+        return null;
     }
-
+  
 }
