a6b88cb1ea13266dd5782b2b6913f34c181fec84
noemis13 <noemischerer13@gmail.com>
2016-04-24 22:56:13 -0300

    buscaDaoReceita #16

--- src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
@@ -1,6 +1,9 @@
 package br.edu.utfpr.recipes.dao;
 
+import br.edu.utfpr.recipes.entidade.Ingrediente;
 import br.edu.utfpr.recipes.entidade.Receita;
+import java.sql.ResultSet;
+import org.hibernate.Query;
 
 /**
  *
@@ -10,6 +13,15 @@ public class DaoReceita extends DaoGenerics<Receita> {
 
     public DaoReceita() {
         super.clazz = Receita.class;
+    }
+
+    public Receita BuscaIngredienteEspecificado(String nomeIngrediente) {
+        Receita receita = null;
+        session = getsession();
+        Query query = session.createQuery("From " + clazz.getSimpleName() + " where lower(nome) =:nome ");
+        receita = (Receita) query.setParameter("nome", nomeIngrediente.toLowerCase()).uniqueResult();
+
+        return receita;
     }
 
 }
