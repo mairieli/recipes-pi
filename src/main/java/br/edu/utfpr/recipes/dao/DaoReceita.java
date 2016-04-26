a8c928806765168ec37e6894ba2a93afd2351ccc
noemis13 <noemischerer13@gmail.com>
2016-04-26 20:56:07 -0300

    Alterando e finalizando buscaDaoReceita #16

--- src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
@@ -1,9 +1,9 @@
 package br.edu.utfpr.recipes.dao;
 
-
 import br.edu.utfpr.recipes.entidade.Ingrediente;
 import br.edu.utfpr.recipes.entidade.Receita;
 import java.util.ArrayList;
+import java.util.List;
 import org.hibernate.Query;
 
 /**
@@ -16,12 +16,27 @@ public class DaoReceita extends DaoGenerics<Receita> {
         super.clazz = Receita.class;
     }
 
-    public ArrayList<Receita> BuscaIngredienteEspecificado(ArrayList<Ingrediente> nomeIngrediente) {
-        ArrayList<Receita> receita = null;
+    public List<Receita> BuscaIngredienteEspecificado(ArrayList<Ingrediente> ingreditentes) {
         session = getsession();
-        Query query = session.createQuery("From " + clazz.getSimpleName() + " where lower(nome) =:nome ");
-        receita = (ArrayList<Receita>) query.setParameter("nome", nomeIngrediente).uniqueResult();
-        return null;
+        String hql = "i.ingrediente.nome = ";
+        if (ingreditentes.isEmpty()) {
+            System.out.println("Lista de ingredientes vazia.");
+        } else {
+            for (int i = 0; i < ingreditentes.size(); i++) {
+                Ingrediente ingrediente = ingreditentes.get(i);
+                if (ingreditentes.size() > 1 && i < ingreditentes.size() - 1) {
+                    hql += "'" + ingrediente.getNome() + "' AND i.ingrediente.nome = ";
+                } else if (i == ingreditentes.size() - 1) {
+                    hql += "'" + ingrediente.getNome() + "'";
+                } else {
+                    hql += "'" + ingrediente.getNome() + "'";
+                }
+            }
+
+        }
+        Query query = session.createQuery("select i.receita FROM ItemReceita i WHERE " + hql);
+        return query.list();
+
     }
 
 }
