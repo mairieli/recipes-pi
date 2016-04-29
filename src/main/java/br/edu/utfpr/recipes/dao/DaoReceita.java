862b33b45e259568d3e270ab919e8131b6956591
Mairieli Wessel <mairieliw@gmail.com>
2016-04-29 14:52:44 -0300

    Busca de receita por ingredientes específicos #40

--- src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoReceita.java
@@ -15,11 +15,12 @@ public class DaoReceita extends DaoGenerics<Receita> {
     }
 
     /**
-     * Método que recupera as receitas que contenham os ingredientes passados 
+     * Método que recupera as receitas que contenham os ingredientes passados
      * por parâmetro.
      *
      * @author Noemi e Mairieli
-     * @param nomesIngrediente lista de nomes dos ingredientes informados pelo usuário
+     * @param nomesIngrediente lista de nomes dos ingredientes informados pelo
+     * usuário
      * @return lista de receitas
      */
     public List<Receita> buscaReceitaPorIngredientes(List<String> nomesIngrediente) {
@@ -38,6 +39,50 @@ public class DaoReceita extends DaoGenerics<Receita> {
                     + "		select id from Ingrediente where nome = '" + nomeIngrediente + "') "
                     + ") ";
         }
+        Query query = session.createSQLQuery(sql).addEntity(Receita.class);
+        return query.list();
+    }
+
+    /**
+     * Método que recupera as receitas que contenham os ingredientes somente
+     * passados por parâmetro.
+     *
+     * @author Mairieli
+     * @param nomesIngrediente lista de nomes dos ingredientes informados pelo
+     * usuário
+     * @return lista de receitas
+     */
+    public List<Receita> buscaReceitaPorIngredientesEspecificos(List<String> nomesIngrediente) {
+        session = getsession();
+        String ingredientesSelecionados = " (select id from Ingrediente where nome = '" + nomesIngrediente.get(0) + "'";
+        nomesIngrediente.remove(0);
+        for (String nomeIngrediente : nomesIngrediente) {
+            ingredientesSelecionados += " or nome = '" + nomeIngrediente + "' ";
+        }
+        ingredientesSelecionados += ") ";
+        String sql = "SELECT * FROM Receita r "
+                + "WHERE r.status = true AND r.id IN "
+                + "	(SELECT DISTINCT IR.receita_id FROM itemReceita IR "
+                + "	 WHERE NOT EXISTS "
+                + "		(SELECT * FROM "
+                + "			(SELECT Ingrediente.id FROM Ingrediente "
+                + "			WHERE Ingrediente.id IN "
+                + "				" + ingredientesSelecionados + ") AS I "
+                + "		WHERE NOT EXISTS "
+                + "            (SELECT * FROM itemReceita IR2 "
+                + "             WHERE IR.receita_id = IR2.receita_id AND IR2.ingrediente_Id = I.id) "
+                + ") AND r.id IN "
+                + "   (SELECT DISTINCT temp.id FROM "
+                + "		(SELECT r.id FROM Receita AS r, itemReceita AS ir "
+                + "		 WHERE r.id = ir.receita_id "
+                + "         AND ir.ingrediente_id IN " + ingredientesSelecionados + ") AS temp "
+                + "	WHERE NOT EXISTS "
+                + "		(SELECT * FROM itemReceita AS it "
+                + "         WHERE it.ingrediente_id NOT IN " + ingredientesSelecionados
+                + "		 AND temp.id=it.receita_id)"
+                + "    )"
+                + ")";
+
         Query query = session.createSQLQuery(sql).addEntity(Receita.class);
         return query.list();
     }
