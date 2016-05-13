33ccca6bb94560d38591d7c11061e8e95eb15798
Mairieli Wessel <mairieliw@gmail.com>
2016-05-13 20:33:10 -0300

    adicionando filtros na consulta de receita #47

--- src/main/java/br/edu/utfpr/recipes/servlets/BuscaReceitasServlet.java
+++ src/main/java/br/edu/utfpr/recipes/servlets/BuscaReceitasServlet.java
@@ -1,10 +1,17 @@
 package br.edu.utfpr.recipes.servlets;
 
 import br.edu.utfpr.recipes.dao.DaoReceita;
+import br.edu.utfpr.recipes.entidade.Receita;
+import br.edu.utfpr.recipes.entidade.Tag;
+import br.edu.utfpr.recipes.entidade.TagReceita;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +31,7 @@ public class BuscaReceitasServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 
@@ -36,7 +43,7 @@ public class BuscaReceitasServlet extends HttpServlet {
             listaIngredientes.addAll(Arrays.asList(ingredientes));
             List receitas = daoReceita.buscaReceitaPorIngredientesEspecificos(listaIngredientes);
             if (receitas.isEmpty()) {
-                request.setAttribute("filtro",pesquisa);
+                request.setAttribute("filtro", pesquisa);
                 request.setAttribute("vazia", "Não há nenhum resultado para sua pequisa");
                 request.getRequestDispatcher("ConsultarReceitas.jsp").forward(request, response);
             } else {
@@ -49,6 +56,41 @@ public class BuscaReceitasServlet extends HttpServlet {
 
         }
 
+    }
+
+    private void filtraReceitas(List<Receita> receitas, String categoria, String dificuldade, List<String> utensilios) {
+        List<Receita> receitaCategoria = new LinkedList<>();
+        List<Receita> receitasDificuldade = new LinkedList<>();
+        List<Receita> receitasUtensilio = new LinkedList<>();
+        for (Receita receita : receitas) {
+            if (!categoria.isEmpty() && receita.getCategoria().equals(categoria)) {
+                receitaCategoria.add(receita);
+            }
+            if (!dificuldade.isEmpty() && receita.getDificuldade().equals(dificuldade)) {
+                receitasDificuldade.add(receita);
+            }
+            if (utensilios != null && !utensilios.isEmpty()) {
+                List<String> utensiliosReceita = new LinkedList<>();
+                for (TagReceita tag : receita.getTagReceitaList()) {
+                    utensiliosReceita.add(tag.getTag().getNome());
+                }
+                if (!utensiliosReceita.isEmpty()) {
+                    if (utensiliosReceita.containsAll(utensilios)) {
+                        receitasUtensilio.add(receita);
+                    }
+                }
+            }
+        }
+
+        if (!categoria.isEmpty()) {
+            receitas.retainAll(receitaCategoria);
+        }
+        if (!dificuldade.isEmpty()) {
+            receitas.retainAll(receitasDificuldade);
+        }
+        if (!utensilios.isEmpty()) {
+            receitas.retainAll(receitasUtensilio);
+        }
     }
 
 }
