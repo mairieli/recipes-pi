a12b4845daabe6a1a1758cecb7d2df02f2d8cbf5
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-05-15 17:23:53 -0300

    Login de usuario #54

--- src/main/java/br/edu/utfpr/recipes/servlets/LoginDeUsuariosServlet.java
+++ src/main/java/br/edu/utfpr/recipes/servlets/LoginDeUsuariosServlet.java
@@ -1,5 +1,8 @@
 package br.edu.utfpr.recipes.servlets;
 
+import br.edu.utfpr.recipes.dao.DaoUsuario;
+import br.edu.utfpr.recipes.entidade.Usuario;
+import br.edu.utfpr.recipes.util.Criptografia;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +18,24 @@ public class LoginDeUsuariosServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("log");
-    }
+        String email = request.getParameter("email").trim();
+        String senha = request.getParameter("senha").trim();
 
+        DaoUsuario daoUsuario = new DaoUsuario();
+        Usuario usuario = daoUsuario.buscaUsuarioPorEmail(email);
+        if (usuario == null) {
+            request.setAttribute("message_error", "Erro: Este e-mail não consta no sistema");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        } else {
+            Criptografia criptografia = new Criptografia();
+            if (usuario.getSenha().equals(criptografia.criptografar(senha))) {
+                request.getSession().setAttribute("usuarioLogado", usuario);
+                request.getSession().setAttribute("message", "Login Realizado com Sucesso!");
+                response.sendRedirect("index.jsp");
+            } else {
+                request.setAttribute("message_error", "Erro: Senha Inválida");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+            }
+        }
+    }
 }
