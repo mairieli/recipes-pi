7742809703395da6efb5b3fcac6d5048d055d863
Quejao <Leotrone@hotmail.com>
2016-06-09 21:48:20 -0300

    Arrumado dao para comentarios #104

--- src/main/java/br/edu/utfpr/recipes/dao/DaoComentario.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoComentario.java
@@ -1,9 +1,14 @@
 package br.edu.utfpr.recipes.dao;
 
+import br.edu.utfpr.recipes.entidade.ComentarioReceita;
+
 /**
  *
  * @author Leonardo
  */
-public class DaoComentario {
-    
+public class DaoComentario extends DaoGenerics<ComentarioReceita> {
+
+    public DaoComentario() {
+        super.clazz = ComentarioReceita.class;
+    }
 }
