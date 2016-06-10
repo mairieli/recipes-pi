e981a19d99ea36f4423ed7af542e9cad4200a6a3
Mairieli Wessel <mairieliw@gmail.com>
2016-06-10 13:12:34 -0300

    Implementar e criar servlet para comentário #108

--- src/main/java/br/edu/utfpr/recipes/controller/ComentarioServlet.java
+++ src/main/java/br/edu/utfpr/recipes/controller/ComentarioServlet.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.utfpr.recipes.controller;
 
+import br.edu.utfpr.recipes.dao.DaoComentario;
+import br.edu.utfpr.recipes.dao.DaoReceita;
+import br.edu.utfpr.recipes.entidade.ComentarioReceita;
+import br.edu.utfpr.recipes.entidade.Receita;
+import br.edu.utfpr.recipes.entidade.Usuario;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author mairieli
- */
 @WebServlet(name = "ComentarioServlet", urlPatterns = {"/ComentarioServlet"})
 public class ComentarioServlet extends HttpServlet {
 
@@ -28,6 +25,27 @@ public class ComentarioServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String textoComentario = request.getParameter("comentario").trim();
+        Integer idReceita = Integer.parseInt(request.getParameter("receita_id"));
+
+        Calendar dataAtual = Calendar.getInstance();
+        dataAtual.setTime(new Date());
+        Usuario usuario = (Usuario) request.getSession().getAttribute("usuarioLogado");
+        DaoReceita daoReceita = new DaoReceita();
+        Receita receita = daoReceita.findById(idReceita);
+
+        ComentarioReceita comentario = new ComentarioReceita();
+        comentario.setComentario(textoComentario);
+        comentario.setDataComentario(dataAtual);
+        comentario.setReceita(receita);
+        comentario.setUsuario(usuario);
+
+        DaoComentario daoComentario = new DaoComentario();
+        daoComentario.save(comentario);
+
+        response.sendRedirect("VisualizacaoReceita.jsp?receita_id=" + receita.getId());
     }
 
 }
