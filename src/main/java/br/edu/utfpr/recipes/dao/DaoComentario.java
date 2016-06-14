e57dd82c6b01d1e204e83c6cc0874dc1fa945d02
noemis13 <noemischerer13@gmail.com>
2016-06-14 19:32:31 -0300

    Busca comentario da receita #109

--- src/main/java/br/edu/utfpr/recipes/dao/DaoComentario.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoComentario.java
@@ -1,6 +1,10 @@
 package br.edu.utfpr.recipes.dao;
 
+import static br.edu.utfpr.recipes.dao.DaoGenerics.session;
 import br.edu.utfpr.recipes.entidade.ComentarioReceita;
+import br.edu.utfpr.recipes.entidade.Receita;
+import java.util.List;
+import org.hibernate.Query;
 
 /**
  *
@@ -11,4 +15,16 @@ public class DaoComentario extends DaoGenerics<ComentarioReceita> {
     public DaoComentario() {
         super.clazz = ComentarioReceita.class;
     }
+    
+    public List<ComentarioReceita> buscaReceitaPorIngredientes(String idReceita) {
+        session = getsession();
+        String sql = "SELECT * "
+                + "FROM ComentarioReceita c "
+                + "WHERE c.id IN ( "
+                + "     select id from Receita where id = '" + idReceita + "') "
+                + ") ";
+        Query query = session.createSQLQuery(sql).addEntity(ComentarioReceita.class);
+        return query.list();
+    }
+
 }
