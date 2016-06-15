c05f9c82f5a37b458ad4bf25aa2c293c7967836c
Josimar Loch <josimarlochcm@gmail.com>
2016-06-15 00:22:08 -0300

    Criar consultas de comentário/receita por Usuário ordenado inversamente #124 (Comentario)

--- src/main/java/br/edu/utfpr/recipes/dao/DaoComentario.java
+++ src/main/java/br/edu/utfpr/recipes/dao/DaoComentario.java
@@ -2,6 +2,7 @@ package br.edu.utfpr.recipes.dao;
 
 import static br.edu.utfpr.recipes.dao.DaoGenerics.session;
 import br.edu.utfpr.recipes.entidade.ComentarioReceita;
+import br.edu.utfpr.recipes.entidade.Usuario;
 import java.util.List;
 import org.hibernate.Query;
 
@@ -14,6 +15,12 @@ public class DaoComentario extends DaoGenerics<ComentarioReceita> {
     public List<ComentarioReceita> buscaComentarioDaReceita(int idReceita) {
         session = getsession();
         Query query = session.createQuery("SELECT c FROM ComentarioReceita c WHERE c.receita.id =" + idReceita + " ORDER BY c.dataComentario DESC");
+        return query.list();
+    }
+    public List<ComentarioReceita> buscaComentarioPorUsuario(Usuario usuario) {
+        session = getsession();
+        Query query = session.createQuery("SELECT c FROM ComentarioReceita c WHERE c.usuario.id =:usuario_id  ORDER BY c.dataComentario DESC");
+        query.setParameter("usuario_id", usuario.getId());
         return query.list();
     }
 
