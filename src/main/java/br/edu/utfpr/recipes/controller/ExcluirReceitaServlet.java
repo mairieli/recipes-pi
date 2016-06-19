0e3fc6a302de0db2ea93a363d7eba14db86e9e4d
Mairieli Wessel <mairieliw@gmail.com>
2016-06-19 18:08:53 -0300

    Excluir comentários antes de excluir receita #127

--- src/main/java/br/edu/utfpr/recipes/controller/ExcluirReceitaServlet.java
+++ src/main/java/br/edu/utfpr/recipes/controller/ExcluirReceitaServlet.java
@@ -1,28 +1,32 @@
 package br.edu.utfpr.recipes.controller;
 
+import br.edu.utfpr.recipes.dao.DaoComentario;
 import br.edu.utfpr.recipes.dao.DaoReceita;
+import br.edu.utfpr.recipes.entidade.ComentarioReceita;
 import br.edu.utfpr.recipes.entidade.Receita;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Leonardo
- */
 @WebServlet(name = "ExcluirReceitaServlet", urlPatterns = {"/ExcluirReceitaServlet"})
 public class ExcluirReceitaServlet extends HttpServlet {
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Integer receita_id = Integer.parseInt(request.getParameter("receita_id"));
-        DaoReceita dao_receita = new DaoReceita();
-        Receita receita = dao_receita.obterParaExcluir(receita_id);
-        dao_receita.delete(receita);
+        Integer receitaId = Integer.parseInt(request.getParameter("receita_id"));
+        DaoComentario daoComentario = new DaoComentario();
+        List<ComentarioReceita> comentarios = daoComentario.buscaComentarioDaReceita(receitaId);
+        for(ComentarioReceita comentario : comentarios){
+            daoComentario.delete(comentario);
+        }
+        DaoReceita daoReceita = new DaoReceita();
+        Receita receita = daoReceita.obterParaExcluir(receitaId);
+        daoReceita.delete(receita);
         request.getSession().setAttribute("message", "Receita Excluida!");
         response.sendRedirect("index.jsp");
     }
