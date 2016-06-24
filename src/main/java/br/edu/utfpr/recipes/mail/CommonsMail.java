94321ebab3b2c41b06dc4ccb611a9edcf0195d63
Josimar Loch <josimarlochcm@gmail.com>
2016-06-24 12:21:16 -0300

    `Correcao bug Email

--- src/main/java/br/edu/utfpr/recipes/mail/CommonsMail.java
+++ src/main/java/br/edu/utfpr/recipes/mail/CommonsMail.java
@@ -2,47 +2,53 @@ package br.edu.utfpr.recipes.mail;
 
 import br.edu.utfpr.recipes.entidade.Receita;
 import br.edu.utfpr.recipes.entidade.Usuario;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
 
 public class CommonsMail {
-
-    /**
-     * envia email simples(somente texto)
-     *
-     * @param usuario
-     * @param receita
-     */
-    public void enviaEmail(Usuario usuario, Receita receita) {
-
-        String conteudo = "Olá " + usuario.getNome() + ", sua receita " + receita.getNome() + " foi aprovada.\n"
-                + "Obrigado, por sua colaboração.\n"
-                + "Atenciosamente,\n"
-                + "Equipe recipes-pi.";
-        SimpleEmail email = new SimpleEmail();
-        email.setHostName("smtp.gmail.com");
-        try {
-            email.addTo(usuario.getEmail());
-
-            email.setFrom("contato.recipes@gmail.com", "Recipes");
-            email.setSubject("Sua receita " + receita.getNome() + " foi aprovada.");
-            email.setMsg(conteudo);
-            email.setAuthentication("contato.recipes@gmail.com", "recipespi2016");
-            email.setSmtpPort(587);
-            email.setSSLOnConnect(true);
-            email.setStartTLSEnabled(true);
-            email.send();
-        } catch (EmailException ex) {
-            System.out.println("Erro ao enviar email " + ex);
-        }
-    }
 
     public void enviaEmailReceita(final Usuario usuario, final Receita receita) {
         new Thread() {
             @Override
             public void run() {
-                enviaEmail(usuario, receita);
+                try {
+                    enviarEmail(usuario, receita);
+                } catch (UnsupportedEncodingException | MessagingException ex) {
+                    System.out.println("!!!!!!!!Erro ao Enviar email!!!!!!");
+                    Logger.getLogger(CommonsMail.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }.start();
+    }
+
+    public void enviarEmail(Usuario usuario, Receita receita) throws UnsupportedEncodingException, MessagingException {
+        String conteudo = "Olá <b>" + usuario.getNome().toUpperCase() + "</b>,<br/> sua receita <b>" + receita.getNome() + "</b> foi aprovada.<br/>"
+                + "Obrigado, por sua colaboração.<br/>"
+                + "Atenciosamente,<br/>"
+                + "Equipe recipes-pi.";
+        MailJava mj = new MailJava();
+        Map<String, String> map = new HashMap<>();
+        map.put(usuario.getEmail(), usuario.getEmail());
+        //configuracoes de envio
+        mj.setSmtpHostMail("mail.loch.net.br");
+        mj.setSmtpPortMail("25");
+        mj.setSmtpAuth("false");
+        mj.setSmtpStarttls("false");
+        mj.setUserMail("recipes-pi@loch.net.br");
+        mj.setFromNameMail("Recipes Pi");
+        mj.setPassMail("recipespi2016");
+        mj.setCharsetMail("ISO-8859-1");
+        mj.setSubjectMail("Sua receita " + receita.getNome() + " foi aprovada.");
+        mj.setBodyMail(conteudo);
+        mj.setTypeTextMail(MailJava.TYPE_TEXT_HTML);
+        mj.setToMailsUsers(map);
+        mj.setFileMails(new ArrayList<String>());
+
+        new MailJavaSender().senderMail(mj);
     }
 }
